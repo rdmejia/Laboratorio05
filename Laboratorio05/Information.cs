c1d1b89b92f80a19de339fec099c24ee464d1ce7
Daniel Mejia <rdmejiaa@gmail.com>
2023-03-21 18:00:32 -0600

    Create tests

---
+++ Laboratorio05/Information.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio05
+{
+    public class Information
+    {
+        private static Information? Instance;
+
+        private string Name;
+        private string Carnet;
+        private string Section;
+
+
+        public static Information GetInformation()
+        {
+            if (Instance == null)
+                Instance = new Information("Daniel Mejia", "999999", "00");
+
+            return Instance;
+        }
+
+        private Information(string Name, string Carnet, string Section)
+        {
+            this.Name = Name;
+            this.Carnet = Carnet;
+            this.Section = Section;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public string GetCarnet()
+        {
+            return this.Carnet;
+        }
+
+        public string GetSection()
+        {
+            return this.Section;
+        }
+    }
+}
---
+++ Laboratorio05Tests/InformationTests.cs
@@ -0,0 +1,36 @@
+namespace Laboratorio05Tests
+{
+    [TestClass]
+    public class InformationTests
+    {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        [TestMethod]
+        public void TestInformation()
+        {
+            var information = Information.GetInformation();
+            
+            Assert.IsNotNull(information, "La informacion es NULL!");
+
+            string name = information.GetName();
+            string carnet = information.GetCarnet();
+            string section = information.GetSection();
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(name), "El nombre no ha sido especificado");
+            Assert.IsFalse(string.IsNullOrWhiteSpace(carnet), "El carnet no ha sido especificado");
+            Assert.IsFalse(string.IsNullOrWhiteSpace(section), "La seccion no ha sido especificada");
+
+            Assert.IsTrue(name.Split(" ").Length >= 2, $"Al menos un nombre y un apellido debe ser especificado: {name}");
+
+            TestContext.WriteLine($"Nombre: {name}");
+            TestContext.WriteLine($"Carnet: {carnet}");
+            TestContext.WriteLine($"Seccion: {section}");
+        }
+    }
+}
