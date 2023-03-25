e538c3a95eeb83acad723035f3e68cc957f033b3
Daniel Mejia <rdmejiaa@gmail.com>
2023-03-24 18:48:03 -0600

    Solve Ejercicio 1

--- Laboratorio05/Equipo.cs
+++ Laboratorio05/Equipo.cs
@@ -1,45 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Laboratorio05
+﻿namespace Laboratorio05
 {
     public class Equipo
     {
+        private string Nombre;
+        private int PartidosGanados;
+        private int PartidosEmpatados;
+        private int PartidosPerdidos;
+        private int GolesFavor;
+        private int GolesContra;
+
         public Equipo(string nombre, int partidosGanados, int partidosEmpatados, int partidosPerdidos, int golesFavor, int golesContra)
         {
+            if (partidosEmpatados + partidosGanados + partidosPerdidos != 3 || golesContra < 0 || golesFavor < 0)
+                throw new Exception("Revise el equipo. La sumatoria de partidos debe ser igual a 3 y los goles anotados y a favor deben ser mayores o iguales a cero.");
+
+            this.Nombre = nombre;
+            this.PartidosGanados = partidosGanados;
+            this.PartidosEmpatados = partidosEmpatados;
+            this.PartidosPerdidos = partidosPerdidos;
+            this.GolesFavor= golesFavor;
+            this.GolesContra = golesContra;
         }
 
         public string GetNombre()
         {
-            return "";
+            return Nombre;
         }
 
         public int GetPartidosGanados()
         {
-            return 0;
+            return PartidosGanados;
         }
 
         public int GetPartidosEmpatados()
         {
-            return 0;
+            return PartidosEmpatados;
         }
 
         public int GetPartidosPerdidos()
         {
-            return 0;
+            return PartidosPerdidos;
         }
 
         public int GetGolesFavor()
         {
-            return 0;
+            return GolesFavor;
         }
 
         public int GetGolesContra()
         {
-            return 0;
+            return GolesContra;
         }
     }
 }
