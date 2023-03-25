90c60537a9d78dc098f9128859ad83b3991bb59d
Daniel Mejia <rdmejiaa@gmail.com>
2023-03-24 22:07:29 -0600

    Solve ejercicio 3

--- Laboratorio05/Torneo.cs
+++ Laboratorio05/Torneo.cs
@@ -10,7 +10,54 @@ namespace Laboratorio05
     {
         public static Equipo[][] SimularTorneo(Equipo[] equipos)
         {
-            return null;
+            if (equipos.Length < 2 || !isPowerOf2(equipos.Length))
+                throw new Exception("La cantidad de equipos debe ser una potencia de dos y deben existir al menos dos equipos");
+
+            int newSize = (int)Math.Log2(equipos.Length) + 1;
+
+            Equipo[][] result = new Equipo[newSize][];
+
+            result[0] = copyArray(equipos, equipos.Length);
+
+            int equiposRestantes = equipos.Length;
+            int fase = 1;
+
+            while (equiposRestantes > 1)
+            {
+                for (int i = 0; i < equiposRestantes / 2; i++)
+                {
+                    Partido partido = new Partido(equipos[i], equipos[equiposRestantes - i - 1]);
+                    Equipo ganador = partido.SeleccionarEquipoGanador();
+                    equipos[i] = ganador;
+                }
+
+                equiposRestantes = equiposRestantes / 2;
+                
+                result[fase] = copyArray(equipos, equiposRestantes);
+
+                fase++;
+            }
+            
+            return result;
+        }
+
+        private static bool isPowerOf2(int x)
+        {
+            double log2 = Math.Log2(x);
+
+            return (log2 - (int)log2) == 0.0;
+        }
+
+        private static Equipo[] copyArray(Equipo[] equipos, int top)
+        {
+            Equipo[] copy = new Equipo[top];
+
+            for (int i = 0; i < top; i++)
+            {
+                copy[i] = equipos[i];
+            }
+
+            return copy;
         }
     }
 }
